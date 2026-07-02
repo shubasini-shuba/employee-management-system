d5b1544b4e02676ea429dae244bb52bdd19ac3d8
Shubasini <shubasini.jmc@gmail.com>
2026-07-02 20:04:18 +0530

    login authentication local storage and navigations are added. employee list page created

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -1,9 +1,49 @@
 import Layout from "../components/layout/layout";
 
 function EmployeeList(){
+  const employees = [
+    {
+      id: 1,
+      name: "Shubashini",
+      department: "Frontend"
+    },
+    {
+      id: 2,
+      name: "Rahul",
+      department: "Backend"
+    },
+    {
+      id: 3,
+      name: "Priya",
+      department: "HR"
+    }
+  ];
   return(
     <Layout>
-    <h1>EmployeeList</h1>
+      <div className="employee-list">
+        <h1>EmployeeList</h1>
+        <table>
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>Name</td>
+              <td>Department</td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              employees.map((employee)=>(
+                <tr key={employee.id}>
+                  <td>{employee.id}</td>
+                  <td>{employee.name}</td>
+                  <td>{employee.department}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    
     </Layout>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
+import {useNavigate} from "react-router-dom";
 import "./login.css";
 
 function Login (){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  function handleLogin(){
+    if(email === "admin@gmail.com" && password === "12345") {
+      localStorage.setItem("isLoggedIn", "true");
+      navigate("/dashboard");
+    } else {
+      console.log("Invalid Credentials");
+    }
+  }
   return(
     <div className="login-container">
       <div className="login-card">
         <h1>Employee Management</h1>
         <h2>Login</h2>
-        <input type="email" placeholder="Enter Email"/>
-        <input type="password" placeholder="Enter Password"/>
-        <button>Login</button>
+        <input type="email" placeholder="Enter Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+        <input type="password" placeholder="Enter Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+        <button onClick={handleLogin}>Login</button>
       </div>
     </div>
   );
